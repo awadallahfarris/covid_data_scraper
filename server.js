44ade89f5ed4f59bf8fdbb2f27aa1977a0d88c1e
Farris Awadallah <awadallah.farris@gmail.com>
2025-01-21 13:57:54 -0500

    First running server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("test for initial push");
+import http from "http";
+import dotenv from 'dotenv'
+
+dotenv.config();
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World\n");
+});
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
